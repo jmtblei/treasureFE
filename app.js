a30fc99e9e1c7072aa8b35b1c27581ad5799bf00
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-09-24 12:43:28 -0700

    starting traverse logic

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ advAxios
         console.log('initializing', res.data)
     })
     .catch(err => console.log('init error', err));
+
+// create new list to record traversal path
+let traversalPath = [];
+
+// create empty graph
+let graph = {};
+
+// define direction to go back upon dead end
+function backwards(dir) {
+    let result = "";
+    if (dir == "n") {
+        result = "s";
+    } else if (dir = "e") {
+        result = "w";
+    } else if (dir = "s") {
+        result = "n";
+    } else if (dir = "w") {
+        result = "e";
+    }
+    return result
+}
+
+// create new list to record backwards movement
+let backwardsPath = [];
