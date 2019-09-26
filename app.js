ebf7d7c727068cd1904e65fa78e3228f782d5a43
Lambda_School_Loaner_191 <lambda_school_loaner_191@lambdasr191smbp.home>
2019-09-26 12:20:45 -0700

    commit

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ advAxios
     .then(res => {
         console.log('initializing', res.data);
         // set the current room to res.data
-        currentRoom = res.data;
-        cooldown = currentRoom.cooldown
+        currentRoom = res.data
     })
     .catch(err => console.log('init error', err));
 
@@ -42,16 +41,15 @@ let backwardsPath = [];
 // looping all rooms until we traverse all 500 rooms
 
 function loopRooms() {
-    let roomNum = currentRoom.room_id
     // add the current room's id to our graph and add it as a new key
-    if (!graph[roomNum]) {
-        graph[roomNum] = {};
+    if (!graph[currentRoom.room_id]) {
+        graph[currentRoom.room_id] = {};
     }
     // add the exits of current room id to graph
     currentRoom.exits.forEach(exit => {
         // if the exit is not listed, add the value as "?" to signify not traversed
-        if (graph[roomNum][exit] == undefined) {
-            graph[roomNum][exit] = "?"
+        if (graph[currentRoom.room_id][exit] == undefined) {
+            graph[currentRoom.room_id][exit] = "?"
         }
     });
 
@@ -60,8 +58,8 @@ function loopRooms() {
 
     // collect list of directions in current room id that hasn't been traversed yet (has value of "?")
     let directions = [];
-    for (let key in graph[roomNum]) {
-        if (graph[roomNum][key] == "?") {
+    for (let key in graph[currentRoom.room_id]) {
+        if (graph[currentRoom.room_id][key] == "?") {
             directions.push(key);
         }
     }
@@ -80,10 +78,11 @@ function loopRooms() {
         setTimeout(() => { // have to settimeout bc there is a cooldown to make request to move again
             advAxios
                 .post('move', { direction: nextMove})
+         
                 .then(res => {
                     console.log('moving to a room', res.data)
                     // save the previous room's id and set it to the current
-                    let prevRoom = roomNum
+                    let prevRoom = currentRoom.room_id
                     currentRoom = res.data;
                     // update the value in graph of prevRoom
                     graph[prevRoom][nextMove] = currentRoom.room_id;
@@ -91,6 +90,7 @@ function loopRooms() {
                     // add new room id to graph
                     if (!graph[newRoom]) {
                         graph[newRoom] = {};
+                        print(graph[newRoom])
                     }
                     // add the value of exits for newRoom
                     currentRoom.exits.forEach(exit => {
@@ -117,10 +117,10 @@ function loopRooms() {
     else if (directions.length == 0 && backwardsPath.length) {
         console.log("this is a dead end or i've already visited this room. tracing my steps backwards now");
         // save the last move and add the backwards move to the end of traversePath
-        let lastMove = backwardsPath.pop()
+        const lastMove = backwardsPath.pop()
         traversalPath.push(lastMove);
-        // save the room id we're moving to as string for wise explorer
-        let lastRoom = graph[roomNum][lastMove].toString();
+        // save the room id we're moving to as string for wise explor   er
+        const lastRoom = graph[currentRoom.room_id][lastMove].toString();
         console.log("lastRoom", lastRoom)
         // send post request to continue moving
         setTimeout(() => {
@@ -129,8 +129,8 @@ function loopRooms() {
                 .then(res => {
                     // set our current room
                     currentRoom = res.data;
-                    cooldown = res.data.cooldown;
-                    console.log("i moved back, i'm now in room:", currentRoom.room_id, "i can move again in:", cooldown)
+                    cooldown = currentRoom.cooldown;
+                    console.log(`i moved back, i'm now in room ${currentRoom.room_id}`)
                     // recursively traverses
                     if (Object.keys(graph).length !== 500) {
                         console.log("that was a dead end. let's go another direction");
@@ -141,10 +141,6 @@ function loopRooms() {
                 })
                 .catch(err => console.log(err.message))
         }, cooldown * 1000) 
-    }
-    else if (directions.length == 0 && backwardsPath.length == 0) {
-        console.log("this is the end of the road", Object.keys(graph).length);
-        return graph;
     }
 }
 
